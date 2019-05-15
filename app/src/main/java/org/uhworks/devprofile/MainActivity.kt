5f17a098ad780e6d30f5dbc147f559de1c3a3def
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2019-05-16 01:23:18 +0200

    Added custom constrained layout for Tablet

--- app/src/main/java/org/uhworks/devprofile/MainActivity.kt
+++ app/src/main/java/org/uhworks/devprofile/MainActivity.kt
@@ -1,12 +1,26 @@
 package org.uhworks.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create a bitmap
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+
+        // Round the bitmap
+        rounded.cornerRadius = 20f
+        //rounded.isCircular = true
+
+        // Set the UI image to the rounded bitmap
+        logoImg.setImageDrawable(rounded)
     }
 }
