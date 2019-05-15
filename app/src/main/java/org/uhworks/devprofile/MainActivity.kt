66a9e9f31e00b40b25001cc2dfa132d52a129e06
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2019-05-15 15:38:35 +0200

    Initial commit: - Phone layout with ConstraintLayout and Chains

---
+++ app/src/main/java/org/uhworks/devprofile/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.uhworks.devprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
